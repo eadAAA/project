a055387e2f765ea2d58fd862f8f313f0b2c0610b
Alibabster <73070525+Alibabster@users.noreply.github.com>
2021-10-12 09:03:22 +0600

    Tolegenov Alibi commit

---
+++ categoryMicroservice/src/main/java/com/microservice/category/api/service/ICategoryService.java
@@ -0,0 +1,50 @@
+package com.microservice.category.api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.microservice.commons.dto.CategoryDTO;
+
+/**
+ * Category service interface
+ * 
+ * @author Marcelo Soares <marceloh.web@gmail.com>
+ *
+ */
+@Service
+public interface ICategoryService {
+
+	/**
+	 * Save a category
+	 * @param category
+	 * @return
+	 */
+	public CategoryDTO insert(CategoryDTO category);
+	
+	/**
+	 * Update a category
+	 * @param category
+	 * @return
+	 */
+	public CategoryDTO update(CategoryDTO category);
+
+	/**
+	 * Get all rows
+	 * @return
+	 */
+	public List<CategoryDTO> getAll();
+	
+	/**
+	 * Get an movie by id
+	 * @return
+	 */
+	public CategoryDTO findById(String id);
+	
+	/**
+	 * Delete a category
+	 * @param id
+	 */
+	public void delete(String id);
+	
+}
